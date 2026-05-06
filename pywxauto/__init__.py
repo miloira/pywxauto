ecb69d48638c17b59c15be595f343ff56aa59708
张明明 <zhangmingming@wnbxyy.com>
2026-05-06 09:08:39 +0800

    update

--- pywxauto/__init__.py
+++ pywxauto/__init__.py
@@ -8,10 +8,4 @@ pywxauto - 微信自动化库
     from pywxauto.wx import TextMessage, ImageMessage, Event
     from pywxauto.wx import SendError, OCRError
 """
-
-try:
-    # 优先加载编译后的 .pyd
-    from pywxauto.wx import *
-except ImportError:
-    # 回退到多模块源码版本
-    from pywxauto.core import *
+from pywxauto.wx import *
